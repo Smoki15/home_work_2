b2ec9d199d379208700442ae2dda9fdc5a8b8a11
Smoki <smoki21098@gmail.com>
2026-03-28 16:43:19 +0200

    domashka_11 and domashka_12

---
+++ domashka_11/Program.cs
@@ -0,0 +1,51 @@
+﻿class Publisher
+{
+    public delegate void Notify(string message);
+
+    public event Notify onNotify;
+
+    public void DoSomething()
+    {
+        Console.WriteLine("Publisher: an event occurred!");
+
+        onNotify?.Invoke("Hello be Publisher!");
+    }
+}
+
+class SubscriberA
+{
+    public void Update(string message)
+    {
+        Console.WriteLine("SubscriberA received a message: " + message);
+    }
+}
+
+class SubscriberB
+{
+    public void Update(string message)
+    {
+        Console.WriteLine("SubscriberB received a message: " + message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Publisher publisher = new Publisher();
+
+        SubscriberA subA = new SubscriberA();
+        SubscriberB subB = new SubscriberB();
+
+        publisher.onNotify += subA.Update;
+        publisher.onNotify -= subB.Update;
+
+        publisher.DoSomething();
+
+        publisher.onNotify -= subA.Update;
+
+        Console.WriteLine("\nAfter unsubscribes SubscriberA:\n");
+
+        publisher.DoSomething();
+    }
+}
---
+++ domashka_12/Program.cs
@@ -0,0 +1,80 @@
+﻿
+class AccountEventArgs
+{
+    public DateTime Date { get; set; }
+    public string User { get; set; }
+    public decimal Amount { get; set; }
+}
+
+class Account
+{
+    public delegate void AccountHandler(AccountEventArgs e);
+
+    public event AccountHandler OnAdded;
+    public event AccountHandler OnWithdrawen;
+
+    public string Owner { get; set; }
+    public decimal Balance { get; private set; }
+
+    public void Add(decimal amount)
+    {
+        Balance += amount;
+
+        OnAdded?.Invoke(new AccountEventArgs
+        {
+            Date = DateTime.Now,
+            User = Owner,
+            Amount = amount
+        });
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if(amount > Balance)
+        {
+            Console.WriteLine("Insufficient funds");
+            return;
+        }
+
+        Balance -= amount;
+
+        OnWithdrawen?.Invoke(new AccountEventArgs
+        {
+            Date = DateTime.Now,
+            User = Owner,
+            Amount = amount
+        });
+    }
+}
+
+class Logger
+{
+    public void OnAddedHandler(AccountEventArgs e)
+    {
+        Console.WriteLine($"[+] {e.Date} | {e.User} topped up the account {e.Amount}");
+    }
+
+    public void OnWithdrawHandler(AccountEventArgs e)
+    {
+        Console.WriteLine($"[-] {e.Date} | {e.User} withdrawn from account {e.Amount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Account acc = new Account { Owner = "Danil" };
+        Logger logger = new Logger();
+
+        acc.OnAdded += logger.OnAddedHandler;
+        acc.OnWithdrawen += logger.OnWithdrawHandler;
+
+        acc.Add(1000);
+        acc.Withdraw(300);
+        acc.Withdraw(800); 
+
+        Console.WriteLine($"Баланс: {acc.Balance}");
+    }
+
+}
